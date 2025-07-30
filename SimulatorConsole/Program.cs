dd49b3df36baafa4176dd697c4a48a3467304f63
Predrag Djukic <66139485+Balein-Fercoadee@users.noreply.github.com>
2025-07-29 20:09:20 -0500

    Number of trials increase in a single simulation (#3)
    * Added pagination to allow for very large trial amounts; implemented event for progress monitoring
    
    * small tweaks to final output

--- SimulatorConsole/Program.cs
+++ SimulatorConsole/Program.cs
@@ -11,7 +11,7 @@ class Program
         // Setup the command line parser.
         RootCommand rootCommand = new RootCommand("A simulator for the two main strategies in the Monty Hall problem.");
 
-        Option<int> trialOption = new Option<int>("--trials")
+        Option<long> trialOption = new Option<long>("--trials")
         {
             Description = $"The number of trials (aka games) to execute in the simulation. The largest value accepted is {Constants.MAX_TRIAL_COUNT}.",
             DefaultValueFactory = parseResult => Constants.DEFAULT_TRIAL_COUNT,
@@ -19,7 +19,7 @@ class Program
         };
         Option<int> threadOption = new Option<int>("--threads")
         {
-            Description = $"The number of threads the simulater will use. The largest value accepted is half the available number of cores, rounded down.",
+            Description = $"The number of threads the simulater will use. The largest value accepted is three-fourths the available number of cores, rounded down.",
             DefaultValueFactory = parseResult => Constants.DEFAULT_THREAD_COUNT,
             HelpName = "numberOfThreads"
         };
@@ -27,7 +27,7 @@ class Program
         rootCommand.Add(trialOption);
         rootCommand.Add(threadOption);
 
-        int numberOfTrials;
+        long numberOfTrials;
         int numberOfThreads;
 
         rootCommand.SetAction(parseResult =>
@@ -40,7 +40,7 @@ class Program
         });
 
         ParseResult parseResult = rootCommand.Parse(args);
-        
+
         Environment.Exit(parseResult.Invoke());
     }
 
@@ -62,9 +62,8 @@ class Program
         Console.WriteLine("Simulation Results");
         Console.WriteLine(new string('=', 50));
         Console.WriteLine($"Simulation runtime: {(endTime - startTime).TotalSeconds:F3} seconds");
-        Console.WriteLine();
         Console.WriteLine("Total number of threads used:");
-        Console.WriteLine($"        {numberOfThreads}");
+        Console.WriteLine($"       {numberOfThreads,14:N0}");
         Console.WriteLine("Total number of game simulated:");
         Console.WriteLine($"        {numberOfTrials,14:N0}");
         Console.WriteLine("Stay with First Door Choice");
@@ -74,17 +73,23 @@ class Program
         Console.WriteLine();
     }
 
-    private static void RunSimulation(int numberOfTrials, int numberOfThreads)
+    private static void RunSimulation(long numberOfTrials, int numberOfThreads)
     {
 
         Console.WriteLine();
-        Console.Write($"Running simulation... ");
+        Console.WriteLine($"Running simulation for {numberOfTrials,14:N0} trials...");
 
         MHSimulator simulator = new MHSimulator();
+        simulator.SimulationProgress += Simulator_SimulationProgress;
         MHSimulationOutcome results = simulator.RunSimulation(numberOfTrials, numberOfThreads);
 
-        Console.WriteLine("complete.");
+        Console.WriteLine("Simulation complete.");
 
         PrintTrailer(results.SimulationStartDateTime, results.SimulationEndDateTime, results.TotalWinsWithStay, results.TotalWinsWithSwitch, results.TotalGamesPlayed, results.TotalThreadsUsed);
+    }
+
+    private static void Simulator_SimulationProgress(object? sender, SimulationProgressEventArgs e)
+    {
+        Console.WriteLine($"{e.CurrentNumberOfCompletedTrials,14:N0} completed...");
     }
 }
